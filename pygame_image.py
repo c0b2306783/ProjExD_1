530f6aba7bee42c3499b4550eac4a31754ff4265
kobayashi <c0b2306783@edu.teu.ac.jp>
2024-11-19 16:35:43 +0900

    練習07 ループ解消

--- pygame_image.py
+++ pygame_image.py
@@ -9,22 +9,24 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png")
+    bg_img = pg.image.load("fig/pg_bg.jpg")  # 背景画像Surfaceを作成する
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")  # こうかとん画像Surafaceを作成する
+    kk_img = pg.transform.flip(kk_img, True, False)  # こうかとんを左右反転させる
     tmr = 0
-    bg_x = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
-        
-        screen.blit(bg_img, [bg_x, 0])
-        screen.blit(bg_img, [bg_x+1600, 0])
-        screen.blit(pg.transform.flip(kk_img, 1, 0), [300, 200])
+        x = tmr%800  # 練習6-2
+        screen.blit(bg_img, [-x, 0])  # screen Surfaceに背景画像Surfaceを貼り付ける
+        x = -(tmr%3200)
+        screen.blit(bg_img,  [x, 0])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img,  [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
+        screen.blit(kk_img, [300, 200])  # screen Surfaceにこうかとん画像Surfaceを貼り付ける
         pg.display.update()
-        tmr += 1
-        bg_x = -(tmr%1600)
-            
+        tmr += 1        
         clock.tick(200)
 
 
@@ -32,4 +34,3 @@ if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exit()
