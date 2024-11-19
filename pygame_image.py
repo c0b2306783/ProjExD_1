cdc323b8f0429f4775a60268fcf10e00d1f48fd7
kobayashi <c0b2306783@edu.teu.ac.jp>
2024-11-19 17:00:35 +0900

    演習01 こうかとんが流される事象

--- pygame_image.py
+++ pygame_image.py
@@ -25,12 +25,17 @@ def main():
         #       key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
         if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
             kk_rct.move_ip(0, -1)
+            
         if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
             kk_rct.move_ip(0, +1)
+            
         if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            kk_rct.move_ip(-1, 0)
+        else:
             kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
             kk_rct.move_ip(+1, 0)
+            kk_rct.move_ip(+2, 0)
 
         x = -(tmr%3200)
         screen.blit(bg_img,  [x, 0])
